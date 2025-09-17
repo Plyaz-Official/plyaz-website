5833335e66dcdfdd382f6a734787a306a824b704
Hiroe Ito <126674600+Hiroeee@users.noreply.github.com>
2025-09-17 17:50:19 +0100

    add quote-box section

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const texts = {
+  home: {
+    engageQuote: 'Engage with a new era of sports investment by tokenizing athletes, joining fantasy leagues, and owning exclusive NFTs. Don’t miss out—join today and elevate your sports experience!'
+  }
+};
