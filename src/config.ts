d5fa35d0bc7f8d66fba073f03661218552b2c1f1
Hiroe Ito <126674600+Hiroeee@users.noreply.github.com>
2025-09-19 22:54:52 +0100

    created grid card content components

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,43 @@
 export const texts = {
   home: {
-    engageQuote: 'Engage with a new era of sports investment by tokenizing athletes, joining fantasy leagues, and owning exclusive NFTs. Don’t miss out—join today and elevate your sports experience!'
+    engageQuote: 'Engage with a new era of sports investment by tokenizing athletes, joining fantasy leagues, and owning exclusive NFTs. Don’t miss out—join today and elevate your sports experience!',
+
+    cardContents: {
+      missionItems: [{
+          topTitle: 'company',
+          bottomTitle: 'mission',
+          description: '',
+        }, {
+          topTitle: 'Breaking down the walls between athletes and fans. Forever.',
+          bottomTitle: '01',
+          description: '',
+        }, {
+          topTitle: 'Where passion meets opportunity, and dedication gets rewarded.',
+          bottomTitle: '02',
+          description: '',
+        }, {
+          topTitle: 'No more watching from the sidelines; become part of their journey.',
+          bottomTitle: '03',
+          description: '',
+        }, {
+          topTitle: 'Your support becomes their success, and their success becomes yours.',
+          bottomTitle: '04',
+          description: '',
+        }, {
+          topTitle: '',
+          bottomTitle: '',
+          description: '',
+        }],
+      revolutionItems: [{
+        topTitle: 'Revolutionary',
+        bottomTitle: 'Features',
+        description: 'Experience the future of sports investment through our cutting-edge platform.',
+        ctaLabel: 'read the doc',
+      }, {
+        topTitle: "The Sports World's Best-Kept Secret",
+        bottomTitle: 'Plyaz ecosystem',
+        description: 'Join industry pros and super fans to discover how careers are built in sports. Connect with key players where passion meets opportunity.',
+      }],
+    },
   }
 };
