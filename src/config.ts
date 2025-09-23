e69457ffd597fdf8246e51a3fd4671b6cd5d2c52
Hiroe Ito <126674600+Hiroeee@users.noreply.github.com>
2025-09-23 14:32:36 +0100

    add grid card system for the hero background

--- src/config.ts
+++ src/config.ts
@@ -1,51 +1,77 @@
-export const texts = {
+import type { Props as Item } from './components/CardGrid/card.astro';
+
+type Texts = {
+  home: {
+    engageQuote: string;
+    cardContents: {
+      missionItems: Item[];
+      revolutionItems: Item[];
+    };
+    heroBackground: {
+      emptyItems: Item[];
+    };
+  }
+}
+
+export const texts: Texts = {
   home: {
     engageQuote: 'Engage with a new era of sports investment by tokenizing athletes, joining fantasy leagues, and owning exclusive NFTs. Don’t miss out—join today and elevate your sports experience!',
 
     cardContents: {
       missionItems: [{
+          type: 'main',
           topTitle: 'company',
           bottomTitle: 'mission',
           description: '',
         }, {
+          type: 'child',
           topTitle: 'Breaking down the walls between athletes and fans. Forever.',
           bottomTitle: '01',
           description: '',
         }, {
+          type: 'child',
           topTitle: 'Where passion meets opportunity, and dedication gets rewarded.',
           bottomTitle: '02',
           description: '',
         }, {
+          type: 'child',
           topTitle: 'No more watching from the sidelines; become part of their journey.',
           bottomTitle: '03',
           description: '',
         }, {
+          type: 'child',
           topTitle: 'Your support becomes their success, and their success becomes yours.',
           bottomTitle: '04',
           description: '',
         }, {
+          type: 'child',
           topTitle: '',
           bottomTitle: '',
           description: '',
         }],
       revolutionItems: [{
+        type: 'main',
         topTitle: 'Revolutionary',
         bottomTitle: 'Features',
         description: 'Experience the future of sports investment through our cutting-edge platform.',
         ctaLabel: 'read the doc',
       }, {
+        type: 'child',
         topTitle: "The Sports World's Best-Kept Secret",
         bottomTitle: 'Plyaz ecosystem',
         description: 'Join industry pros and super fans to discover how careers are built in sports. Connect with key players where passion meets opportunity.',
       }, {
+        type: 'child',
         topTitle: 'Your Talent Deserves Recognition',
         bottomTitle: 'Athlete tokenization',
         description: 'Turn your athletic talent into unique fan experiences. Let scouts and clubs vie for your attention while you build your authentic personal brand and get rewarded for your dedication.',
       }, {
+        type: 'child',
         topTitle: 'Level Up Your Fan Status',
         bottomTitle: 'Fan engagement',
         description: 'Take on challenges to show your super fan status. Unlock exclusive content and connect with athletes - Be part of their journey and celebrations.',
       }, {
+        type: 'child',
         topTitle: 'Fantasy Powered by Real Relationships',
         bottomTitle: 'Fantasy sports',
         description: 'Create your fantasy team with athletes you support. Your passion boosts their performance. Join leagues where fandom meets strategy.',
@@ -54,41 +80,40 @@ export const texts = {
 
     heroBackground: {
       emptyItems: [{
-        topTitle: '',
-        bottomTitle: '',
-        description: '',
+        type: 'signature',
+        label: {
+          value: 'new era of sports',
+          position: 'top-left',
+        },
       },{
-        topTitle: '',
-        bottomTitle: '',
-        description: '',
+        type: 'signature',
       },{
-        topTitle: '',
-        bottomTitle: '',
-        description: '',
+        type: 'signature',
+        label: {
+          value: 'create opportunities',
+          position: 'bottom-right',
+        },
       },{
-        topTitle: '',
-        bottomTitle: '',
-        description: '',
+        type: 'signature',
       },{
-        topTitle: '',
-        bottomTitle: '',
-        description: '',
+        type: 'signature',
       },{
-        topTitle: '',
-        bottomTitle: '',
-        description: '',
+        type: 'signature',
+        label: {
+          value: 'empowering athletes',
+          position: 'bottom-left',
+        },
       },{
-        topTitle: '',
-        bottomTitle: '',
-        description: '',
+        type: 'signature',
+        label: {
+          value: 'connecting talents',
+          position: 'top-right',
+        },
       },{
-        topTitle: '',
-        bottomTitle: '',
-        description: '',
+        type: 'signature',
       },{
-        topTitle: '',
-        bottomTitle: '',
-        description: '',
+        type: 'signature',
+        showSignature: true,
       },]
     } ,
   }
