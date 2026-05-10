8ab88e1ca0bb4e18f07057e047715f318c421cbc
Ian Silva <Iansilva@plyaz.co.uk>
2026-05-10 23:20:52 +0100

    design: truncate descriptions and limit tags on blog cards

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,40 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_Da6_O6Jl.mjs';
+import { manifest } from './manifest_CkAPILko.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/api/keystatic/_---params_.astro.mjs');
+const _page2 = () => import('./pages/insights.astro.mjs');
+const _page3 = () => import('./pages/insights/_---slug_.astro.mjs');
+const _page4 = () => import('./pages/keystatic/_---params_.astro.mjs');
+const _page5 = () => import('./pages/legal/_---slug_.astro.mjs');
+const _page6 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["node_modules/@keystatic/astro/internal/keystatic-api.js", _page1],
+    ["src/pages/insights/index.astro", _page2],
+    ["src/pages/insights/[...slug].astro", _page3],
+    ["node_modules/@keystatic/astro/internal/keystatic-astro-page.astro", _page4],
+    ["src/pages/legal/[...slug].astro", _page5],
+    ["src/pages/index.astro", _page6]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "ffe7cce2-6597-4746-a430-5ff8e7d7d9c8",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (Object.prototype.hasOwnProperty.call(serverEntrypointModule, _start)) ;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
