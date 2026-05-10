ddc2678d3421ada69d8e114fa106f271e38755a9
Ian Silva <Iansilva@plyaz.co.uk>
2026-05-10 19:57:17 +0100

    fix: v1 vercel serverless crash due to keystatic local storage

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,8 +1,11 @@
 import { config, fields, collection } from '@keystatic/core';
 
 export default config({
-  storage: {
+  storage: process.env.NODE_ENV === 'development' ? {
     kind: 'local',
+  } : {
+    kind: 'github',
+    repo: 'Plyaz-Official/plyaz-website'
   },
   collections: {
     insights: collection({
